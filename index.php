4b22de268e87ae18d90c47c9320fe79b2678796c
Marilyne Naili <marilyne.lydia@gmail.com>
2023-10-05 22:33:49 +0200

    update navBar and add card

--- index.php
+++ index.php
@@ -2,10 +2,52 @@
 <body> <header> <?php navbar($page); ?> </header>
 
 <main>
-
-    <p>Exemple</p>
-
+    <div class="accueil">
+        
+        <img src="https://edgard-lelegant.com/wp-content/uploads/2019/08/lefteris-kallergis-j1GiPlvSGWI-unsplash-scaled.jpg" class="px-5" alt="Responsive image">
+        <button class="btn btn-dark btn-lg" style="top: 50%; left: 50%; transform: translate(50%, -250%);">Shop now</button>
+    </div>
 </main>
+
+<div class="row justify-content-around">
+    <div class="card" style="width: 18rem;">
+        <h3 class="p-4 text-center">NIKE SACAI</h3>
+        <p class="card-text text-center">MEN'S TRAINING SHOES</p>
+        <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
+        <div class="card-body">
+            <p class="card-text text-center">€ 130</p>
+        </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+        <h3 class="p-4 text-center">NIKE SACAI</h3>
+        <p class="card-text text-center">MEN'S TRAINING SHOES</p>
+        <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
+        <div class="card-body">
+            <p class="card-text text-center">€ 130</p>
+        </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+        <h3 class="p-4 text-center">NIKE SACAI</h3>
+        <p class="card-text text-center">MEN'S TRAINING SHOES</p>
+        <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
+        <div class="card-body">
+            <p class="card-text text-center">€ 130</p>
+        </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+        <h3 class="p-4 text-center">NIKE SACAI</h3>
+        <p class="card-text text-center">MEN'S TRAINING SHOES</p>
+        <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
+        <div class="card-body">
+            <p class="card-text text-center">€ 130</p>
+        </div>
+    </div>
+    
+
+</div>
 
 
 </body> </html>
