97b57d86fb572d5a8b80cb8af582ca076d3c46e7
TheMaximeSan <mxm.tournier@gmail.com>
2023-10-05 16:59:55 +0200

    update

---
+++ products.php
@@ -0,0 +1,13 @@
+<?php include "app/app.php"; $page = 2; head($page); ?>
+<body> <header> <?php navbar($page); ?> </header>
+
+<main>
+
+    <p>Exemple</p>
+
+    <i class="fas fa-folder"></i>
+
+</main>
+
+
+</body> </html>
