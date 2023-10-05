659c1c6ec57f2f41ad993349a9d9591b406571da
Maxime Tournier <mxm.tournier@gmail.com>
2023-10-05 12:16:39 +0200

    update navbar

--- app/extension.php
+++ app/extension.php
@@ -2,4 +2,6 @@
 include "env.php";
 
 require_once $env_urlFile . "Boostrap.phtml";
+require_once $env_urlFile . "FontAwesone.phtml";
+require_once $env_urlFile . "Bulma.phtml";
 require_once $env_urlFile . "link.phtml";
