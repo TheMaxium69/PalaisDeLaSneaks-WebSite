3e42102f94d7e1af13630cc6847d3ab862202110
Marilyne Naili <marilyne.lydia@gmail.com>
2023-10-06 09:26:23 +0200

    update navBar

--- index.php
+++ index.php
@@ -9,7 +9,18 @@
     </div>
 </main>
 
-<div class="row justify-content-around">
+<h1>Nouveauté</h1>
+
+<div class="row justify-content-around px-5">
+    <div class="card" style="width: 18rem;">
+        <h3 class="p-4 text-center">NIKE SACAI</h3>
+        <p class="card-text text-center">MEN'S TRAINING SHOES</p>
+        <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
+        <div class="card-body">
+            <p class="card-text text-center">€ 130</p>
+        </div>
+    </div>
+
     <div class="card" style="width: 18rem;">
         <h3 class="p-4 text-center">NIKE SACAI</h3>
         <p class="card-text text-center">MEN'S TRAINING SHOES</p>
