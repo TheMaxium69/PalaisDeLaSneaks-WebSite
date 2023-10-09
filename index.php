f407eb4aad88395a8fff6afbe669c6085c6d0521
TheMaximeSan <mxm.tournier@gmail.com>
2023-10-09 12:44:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
-<?php include "app/app.php";
-$page = 1;
-head($page); ?>
-
-<body>
-    <header> <?php navbar($page); ?> </header>
+<?php include "app/app.php"; $page = 1; head($page); ?>
+<body> <header> <?php navbar($page); ?> </header>
+<?php require_once 'api/whmcs/connect.php'; $productAll = product(); ?>
 
     <main>
         <div class="accueil">
@@ -13,7 +10,6 @@ head($page); ?>
             </div>
         </div>
 
-
         <section id="gallery">
 
 
@@ -21,87 +17,23 @@ head($page); ?>
 
             <div class="row justify-content-around px-5">
 
-                <div class="card" style="width: 18rem;">
-                    <h3 class="p-4 text-center">Je suis une chaussure</h3>
-                    <p class="card-text text-center">MEN'S TRAINING SHOES</p>
-                    <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
-                    <div class="card-body">
-                        <p class="card-text text-center">€ 130</p>
-                    </div>
-                </div>
 
-                <div class="card" style="width: 18rem;">
-                    <h3 class="p-4 text-center">Je suis une chaussure</h3>
-                    <p class="card-text text-center">MEN'S TRAINING SHOES</p>
-                    <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
-                    <div class="card-body">
-                        <p class="card-text text-center">€ 130</p>
+                <?php
+
+                $products = $productAll['product'];
+                for ($i = 0; $i <= 9; $i++) {
+                    ?>
+
+                    <div class="card" style="width: 18rem;">
+                        <h3 class="p-4 text-center"><?= $products[$i]['name'] ?></h3>
+                        <p class="card-text text-center">MEN'S TRAINING SHOES</p>
+                        <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text text-center">€ 130</p>
+                        </div>
                     </div>
-                </div>
-                <div class="card" style="width: 18rem;">
-                    <h3 class="p-4 text-center">Je suis une chaussure</h3>
-                    <p class="card-text text-center">MEN'S TRAINING SHOES</p>
-                    <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
-                    <div class="card-body">
-                        <p class="card-text text-center">€ 130</p>
-                    </div>
-                </div>
-                <div class="card" style="width: 18rem;">
-                    <h3 class="p-4 text-center">Je suis une chaussure</h3>
-                    <p class="card-text text-center">MEN'S TRAINING SHOES</p>
-                    <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
-                    <div class="card-body">
-                        <p class="card-text text-center">€ 130</p>
-                    </div>
-                </div>
-                <div class="card" style="width: 18rem;">
-                    <h3 class="p-4 text-center">Je suis une chaussure</h3>
-                    <p class="card-text text-center">MEN'S TRAINING SHOES</p>
-                    <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
-                    <div class="card-body">
-                        <p class="card-text text-center">€ 130</p>
-                    </div>
-                </div>
-                <div class="card" style="width: 18rem;">
-                    <h3 class="p-4 text-center">Je suis une chaussure</h3>
-                    <p class="card-text text-center">MEN'S TRAINING SHOES</p>
-                    <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
-                    <div class="card-body">
-                        <p class="card-text text-center">€ 130</p>
-                    </div>
-                </div>
-                <div class="card" style="width: 18rem;">
-                    <h3 class="p-4 text-center">Je suis une chaussure</h3>
-                    <p class="card-text text-center">MEN'S TRAINING SHOES</p>
-                    <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
-                    <div class="card-body">
-                        <p class="card-text text-center">€ 130</p>
-                    </div>
-                </div>
-                <div class="card" style="width: 18rem;">
-                    <h3 class="p-4 text-center">Je suis une chaussure</h3>
-                    <p class="card-text text-center">MEN'S TRAINING SHOES</p>
-                    <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
-                    <div class="card-body">
-                        <p class="card-text text-center">€ 130</p>
-                    </div>
-                </div>
-                <div class="card" style="width: 18rem;">
-                    <h3 class="p-4 text-center">Je suis une chaussure</h3>
-                    <p class="card-text text-center">MEN'S TRAINING SHOES</p>
-                    <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
-                    <div class="card-body">
-                        <p class="card-text text-center">€ 130</p>
-                    </div>
-                </div>
-                <div class="card" style="width: 18rem;">
-                    <h3 class="p-4 text-center">Je suis une chaussure</h3>
-                    <p class="card-text text-center">MEN'S TRAINING SHOES</p>
-                    <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
-                    <div class="card-body">
-                        <p class="card-text text-center">€ 130</p>
-                    </div>
-                </div>
+
+                <?php } ?>
 
             </div>
 
