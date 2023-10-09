c7fcd218f1a43922125fb2513a3d720a037deb55
Nueve69 <yder09@gmail.com>
2023-10-09 15:40:50 +0200

    Formule nettoyage

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
+    console.log("je suis un test");
     const cartes = document.querySelectorAll(".carte");
-    const carteDetail = document.querySelector(".carte-detail .description");
+    const carteDetail = document.querySelector(".carte-detail");
+    const carteDetailDescri = document.querySelector(".carte-detail .description");
+
+    // Variable pour suivre la visibilité de la carte détaillée
+    let isCarteDetailVisible = false;
 
     cartes.forEach((carte) => {
-        const titre = carte.querySelector("h2");
-        const description = carte.querySelector("p");
-
+        console.log(carte);
         carte.addEventListener("click", () => {
-            const titreTexte = titre.textContent;
-            const descriptionTexte = description.textContent;
+            const titre = carte.getAttribute("data-title");
+            const description = carte.getAttribute("data-description");
 
-            carteDetail.innerHTML = `<h2>${titreTexte}</h2><p>${descriptionTexte}</p>`;
-            carteDetail.classList.remove("hidden");
+            // Vérifier si la carte détaillée est visible
+            if (isCarteDetailVisible) {
+                // Si elle est visible, masquer la carte détaillée
+                carteDetail.classList.remove("carte-active");
+                isCarteDetailVisible = false;
+            } else {
+                // Si elle n'est pas visible, afficher la carte détaillée et mettre à jour son contenu
+                carteDetailDescri.innerHTML = `<h2>${titre}</h2><p>${description}</p>`;
+                carteDetail.classList.add("carte-active");
+                isCarteDetailVisible = true;
+            }
         });
     });
 });
--- nettoyage.php
+++ nettoyage.php
@@ -3,45 +3,46 @@
 <body> <header> <?php navbar($page); ?> </header>
 
 <main>
-
-    <h1>Nettoyage</h1>
-    <br>
-    <br>
-    <img src="https://static.nike.com/a/images/f_auto,cs_srgb/w_1920,c_limit/337f57b2-0272-4410-93c5-82c2c0f007a2/nettoyer-ses-chaussures-en-6%C2%A0%C3%A9tapes-faciles.jpg" alt="nettoyage">
-<div class= "row d-flex justify-content-center">
-    <div class="cartes-container">
-    <div class="cartes-container">
-            <div class="carte">
-                <h2>Formule 1</h2>
-                <p>Description de la formule 1</p>
-            </div>
+        <h1>Nettoyage</h1>
+        <br>
+        <br>
+        <img src="https://static.nike.com/a/images/f_auto,cs_srgb/w_1920,c_limit/337f57b2-0272-4410-93c5-82c2c0f007a2/nettoyer-ses-chaussures-en-6%C2%A0%C3%A9tapes-faciles.jpg" alt="nettoyage">
+        <div class="row d-flex justify-content-center">
+            <div class="cartes-container">
+                <div class="carte" data-title="Formule 1" data-description="Description de la formule 1">
+                    <h2>Formule 1</h2>
+                    <p>Description de la formule 1</p>
+                </div>
             
-            <div class="carte">
-                <h2>Formule 2</h2>
-                <p>Description de la formule 2</p>
-            </div>
+                <div class="carte" data-title="Formule 2" data-description="Description de la formule 2">
+                    <h2>Formule 2</h2>
+                    <p>Description de la formule 2</p>
+                </div>
             
-            <div class="carte">
-                <h2>Formule 3</h2>
-                <p>Description de la formule 3</p>
+                <div class="carte" data-title="Formule 3" data-description="Description de la formule 3">
+                    <h2>Formule 3</h2>
+                    <p>Description de la formule 3</p>
+                </div>
             </div>
         </div>
-        </div>
-    </div>
-    
-</div>
-<div class="carte-container">
-    <div class="carte-detail hidden">
+        
+        <div class="row d-flex justify-content-center">
+        <div class="carte-d"> <div class="carte-detail">
             <h2>Formule détaillée</h2>
             <p class="description"></p>
+            <button type="button" class="btn btn-primary">je choisis celle-la !</button>
         </div>
-</main>
+        </div>
+       
+
+        </div>
+        
+    </main>
+
 
 
    
+<script src="javascript/script.js"></script>
 
 <footer> <?php footer($page); ?> </footer>
-
-
-<script src="javascript/script.js"></script>
 </body> </html>
