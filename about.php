ed1969de5d47f7e34365d2dfe7fd9ac24a689309
TheMaximeSan <mxm.tournier@gmail.com>
2023-10-10 08:46:45 +0200

    update

--- about.php
+++ about.php
@@ -3,8 +3,120 @@
 
 <main>
 
-    <p>Therms</p>
+    <h1 style="text-align: center; margin: 40px;">Terme d'utilisation</h1>
+
+    <hr>
 
 </main>
+<section id="info">
+    <div id="info" class="container" style="text-align: start;">
+        <div style="text-align: center">
+            <h2 style="text-align: center; margin-bottom: 20px; color: #bb2d3b"><i class="fas fa-scroll"></i>Administratif de l'entreprise</h2>
+            <p>Palais de la sneaks est une entreprise à but lucratif, toute création de l'entreprise nous appartient.</p>
+            <p>Fondé par </p>
+            <p>Siège Social : <br> 52 boulevard des castors, 69005 Lyon</p>
+            <p>Contact : <br> palaisdelasneaks@gmail.com <br> +33 6 51 72 66 93</p>
+        </div>
+        <h2 style="margin-top: 20px;text-align: center; color: #bb2d3b"><i class="fas fa-paperclip"></i>Crédit du Site Web</h2>
+        <div class="row">
+            <div class="col-6 textmore">
+                <h3 style="margin-bottom: 12px; margin-top: 20px; color: #ff3d50">Sources</h3>
+                <p class="no-p">Développement</p>
+                <small>- <a href="https://tyrolium.fr">Tyrolium (© 2017-2023 Tyrolium, Ent.)</a></small><br>
+                <p class="no-p">Web-Designeur</p>
+                <small>- <a href="https://tyrolium.fr">Tyrolium (© 2017-2023 Tyrolium, Ent.)</a></small><br>
+<!--                <p class="no-p">Hebergeur (WebSite)</p>-->
+<!--                <small>- <a href="https://solidserv.fr">SolidServ ® (© 2017-2023 Tyrolium, Ent.)</a></small><br>-->
+<!--                <p class="no-p">Hebergeur (DataBase)</p>-->
+<!--                <small>- OvhCloud (© Copyright 1999-2023 OVHcloud)</small><br>-->
+            </div>
+            <div class="col-6 textmore">
+                <h3 style="margin-bottom: 12px; margin-top: 20px;color: #ff3d50">Technologies</h3>
+                <p class="no-p">FrameWork</p>
+                <small>- <a href="https://github.com/TheMaxium69/TyroSiteFrameWork">TyroSiteFrameWork-1.0.0</a> </small><br>
+                <p class="no-p">Librairies</p>
+                <small>- Whmcs-8.2.1</small><br>
+                <small>- Bootstrap-5.3.2 </small><br>
+                <small>- FontAwesome-6.4.2</small><br>
+                <p class="no-p">Extentions</p>
+                <small>- PhpMyAdmin</small><br>
+                <small>- MySQL</small><br>
+                <p class="no-p">Languages</p>
+                <small>- HTML</small><br>
+                <small>- CSS</small><br>
+                <small>- JavaScript</small><br>
+                <small>- PHP</small><br>
+                <small>- SQL</small><br>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div style="text-align: start">
+                <h2 style="text-align: center; margin-bottom: 20px; color: #bb2d3b"><i class="fas fa-globe"></i>Conditions Générales d'Utilisation</h2>
 
-</body> </html>
+                <p>Les présentes Conditions Générales d'Utilisation (ci-après dénommées les "CGU") régissent l'utilisation du site web <strong>Palais de la sneaks</strong> (ci-après dénommé le "Site") et des services associés (ci-après dénommés les "Services"). Veuillez lire attentivement ces CGU avant d'utiliser le Site. En accédant au Site ou en utilisant les Services, vous acceptez d'être lié par ces CGU.</p>
+
+                <h3 style="color: #ff3d50">1. Acceptation des CGU</h3>
+
+                <p>En accédant au Site ou en utilisant les Services, vous déclarez avoir lu, compris et accepté les présentes CGU. Si vous n'acceptez pas ces CGU, veuillez ne pas utiliser le Site.</p>
+
+                <h3 style="color: #ff3d50">2. Utilisation du Site</h3>
+
+                <p>Vous pouvez utiliser le Site et les Services à des fins personnelles et non commerciales, conformément aux lois et réglementations en vigueur. Vous vous engagez à ne pas utiliser le Site de manière frauduleuse, abusive ou illégale.</p>
+
+                <h3 style="color: #ff3d50">3. Propriété intellectuelle</h3>
+
+                <p>Le Site et son contenu, y compris mais sans s'y limiter, les textes, les images, les graphiques, les logos et les vidéos, sont protégés par des droits de propriété intellectuelle et appartiennent à <strong>Palais de la sneaks</strong>. Vous ne pouvez pas copier, modifier, distribuer ou reproduire le contenu du Site sans autorisation préalable.</p>
+
+                <h3 style="color: #ff3d50">4. Limitation de responsabilité</h3>
+
+                <p>Le Site et les Services sont fournis "tels quels", sans garantie d'aucune sorte, expresse ou implicite. <strong>Palais de la sneaks</strong> ne saurait être tenue responsable des dommages directs, indirects, spéciaux, consécutifs ou punitifs résultant de l'utilisation ou de l'incapacité d'utiliser le Site ou les Services.</p>
+
+                <h3 style="color: #ff3d50">5. Liens externes</h3>
+
+                <p>Le Site peut contenir des liens vers des sites web tiers. <strong>Palais de la sneaks</strong> n'exerce aucun contrôle sur ces sites et n'assume aucune responsabilité quant à leur contenu ou à leur utilisation. L'accès à ces liens externes se fait à vos propres risques.</p>
+
+                <h3 style="color: #ff3d50">6. Modification des CGU</h3>
+
+                <p><strong>Palais de la sneaks</strong> se réserve le droit de modifier ces CGU à tout moment, sans préavis. Les modifications entreront en vigueur dès leur publication sur le Site. Il vous incombe de consulter régulièrement les CGU pour vous tenir informé des éventuelles mises à jour.</p>
+
+                <h3 style="color: #ff3d50">7. Droit applicable et juridiction compétente</h3>
+
+                <p>Les présentes CGU sont régies et interprétées conformément aux lois <strong>Française</strong>. Tout litige découlant de l'utilisation du Site ou des Services relèvera de la compétence exclusive du tribunal de grande instance de <strong>Lyon</strong>.</p>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+
+</main>
+<style>
+    div#info{
+        padding: 30px 60px
+    }
+
+    @media (max-width: 496px) {
+        div#info{
+            padding: 30px 20px
+        }
+
+    }
+    @media (max-width: 751px) {
+        div.textmore{
+            width: 100%;
+            flex: 0 0 100%;
+            max-width: 100%;
+        }
+
+    }
+
+    #info a{
+        text-decoration: none;
+        color: black;
+    }
+
+</style>
+
+<footer> <?php footer($page); ?> </footer>
+</body></html>
