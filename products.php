cc79f1a7e6cdedb737f8dbd0f38a49cf76a830e2
Marilyne Naili <marilyne.lydia@gmail.com>
2023-10-10 20:15:11 +0200

    update card page sneakers

--- products.php
+++ products.php
@@ -35,12 +35,15 @@ head($page); ?>
                 <div class="row justify-content-around px-5" id="tableTEST">
                     <?php foreach ($productAll['product'] as $product) {
                         $productPicture = getPicture($product['pid']);
-                        ?>
+                    ?>
                         <div class="card card__one" style="width: 18rem;" onclick="window.location.href = '?p=<?= $product['pid'] ?>';">
                             <h3 class="p-4 text-center"><?= $product['name'] ?></h3>
                             <p class="card-text text-center">MEN'S TRAINING SHOES</p>
-<!--                            <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">-->
-                            <img class="card-img-top" src="api/more/uploads/<?= $productPicture['picture1'] ?>" alt="Card image cap">
+                            <!--                            <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">-->
+                            <div class="cardSneaker" style="height: 200px; background-image: url('api/more/uploads/<?= $productPicture['picture1'] ?>')">
+
+                            </div>
+                            <!-- <img class="card-img-top" src="api/more/uploads/<?= $productPicture['picture1'] ?>" alt="Card image cap"> -->
                             <div class="card-body">
                                 <p class="card-text text-center text-danger"><?= $product['pricing']['EUR']['monthly'] ?> €</p>
                             </div>
