a28d675bca8271c964b39eed38a0cd9bbd3adbb9
ElPodemos <elias.poderr@gmail.com>
2023-10-10 15:20:47 +0200

    finish script nettoyage

--- api/whmcs/connect.php
+++ api/whmcs/connect.php
@@ -15,7 +15,7 @@ function product()
 {
     $command = 'GetProducts';
     $postData = array();
-    $adminUsername = 'TheMaxium69';
+    $adminUsername = 'Ayaaaaaaaaaaaa';
 
     $results = localAPI($command, $postData, $adminUsername);
 
--- nettoyage.php
+++ nettoyage.php
@@ -7,36 +7,100 @@ head($page); ?>
 
     <main>
         <h1 class="mt-5">Service de Nettoyage</h1>
-        <img src="assets/sneakers.jpg" alt="nettoyage">
-        <div class="row d-flex justify-content-center">
-            <div class="cartes-container">
-                <div class="carte" data-title="Clean Premium" data-description="Description de la formule 1">
-                    <h4 id="itemFormule">Clean Premium</h4>
-                </div>
 
-                <div class="carte" data-title="Clean Medium" data-description="Description de la formule 2">
-                    <h4 id="itemFormule">Clean Medium</h4>
-                </div>
+        <img id="imgPremium" class="" src="https://www.apyart.com/1220-large_default/rouge-de-securite-500ml.jpg" alt="nettoyage">
+        <img id="imgMedium" class="hiddenNett" src="https://www.apyart.com/2804-large_default/bleu-de-cobalt.jpg" alt="nettoyage">
+        <img id="imgStandard" class="hiddenNett" src="https://www.apyart.com/1458-large_default/vert-pur-500ml-peinture-acrylique.jpg" alt="nettoyage">
 
-                <div class="carte" data-title="Clean Standard" data-description="Description de la formule 3">
-                    <h4 id="itemFormule">Clean Standard</h4>
-                </div>
+        <br><br>
+
+        <div class="topcoat-button-bar d-flex justify-content-center">
+            <div class="topcoat-button-bar__item">
+                <button onclick="nettoyageChange('Premium')" class="topcoat-button-bar__button" id="Premium">Clean Premium</button>
+            </div>
+            <div class="topcoat-button-bar__item">
+                <button onclick="nettoyageChange('Medium')" class="topcoat-button-bar__button" id="Medium">Clean Medium</button>
+            </div>
+            <div class="topcoat-button-bar__item">
+                <button onclick="nettoyageChange('Standard')" class="topcoat-button-bar__button" id="Standard">Clean Standard</button>
             </div>
         </div>
 
-        <div class="row d-flex justify-content-center">
-            <div class="carte-d">
-                <div class="carte-detail">
-                    <p class="description"></p>
-                    <button type="button" class="btn btn-primary">Je choisis celle-la !</button>
-                </div>
+        <br><br>
+
+        <div class="card">
+            <div id="textPremium" class="card-body">
+                <h5 class="card-title">Clean Premium</h5>
+                <p class="card-text">PremiumPremiumPremiumPremiumPremiumPremiumPremiumPremiumPremiumPremium</p>
+                <p class="card-text">PremiumPremiumPremiumPremiumPremiumPremiumPremiumPremiumPremiumPremium</p>
+                <p class="card-text">PremiumPremiumPremiumPremiumPremiumPremiumPremiumPremiumPremiumPremium</p>
             </div>
+            <div id="textMedium" class="card-body hiddenNett">
+                <h5 class="card-title">Clean Medium</h5>
+                <p class="card-text">MediumMediumMediumMediumMediumMediumMediumMediumMediumMediumMedium</p>
+                <p class="card-text">MediumMediumMediumMediumMediumMediumMediumMediumMediumMediumMedium</p>
+                <p class="card-text">MediumMediumMediumMediumMediumMediumMediumMediumMediumMediumMedium</p>
+            </div>
+            <div id="textStandard" class="card-body hiddenNett">
+                <h5 class="card-title">Clean Standard</h5>
+                <p class="card-text">StandardStandardStandardStandardStandardStandardStandardStandardStandard</p>
+                <p class="card-text">StandardStandardStandardStandardStandardStandardStandardStandardStandard</p>
+                <p class="card-text">StandardStandardStandardStandardStandardStandardStandardStandardStandard</p>
+            </div>
+        </div>
 
-            <!-- CARROUSEL -->
+        <style>
+            .hiddenNett {
+                display: none;
+            }
+        </style>
 
-            <!-- / CARROUSEL -->
+        <script>
+            var imgPremium = document.querySelector("#imgPremium");
+            var imgMedium = document.querySelector("#imgMedium");
+            var imgStandard = document.querySelector("#imgStandard");
 
-            <?php socialLink($page); ?>
+            var textPremium = document.querySelector("#textPremium");
+            var textMedium = document.querySelector("#textMedium");
+            var textStandard = document.querySelector("#textStandard");
+
+            function nettoyageChange($name) {
+                switch ($name) {
+                    case 'Premium':
+                        imgPremium.classList.remove("hiddenNett");
+                        imgMedium.classList.add("hiddenNett");
+                        imgStandard.classList.add("hiddenNett");
+
+                        textPremium.classList.remove("hiddenNett");
+                        textMedium.classList.add("hiddenNett");
+                        textStandard.classList.add("hiddenNett");
+                        break;
+                    case 'Medium':
+                        imgMedium.classList.remove("hiddenNett");
+                        imgPremium.classList.add("hiddenNett");
+                        imgStandard.classList.add("hiddenNett");
+
+                        textMedium.classList.remove("hiddenNett");
+                        textPremium.classList.add("hiddenNett");
+                        textStandard.classList.add("hiddenNett");
+                        break;
+                    case 'Standard':
+                        imgStandard.classList.remove("hiddenNett");
+                        imgPremium.classList.add("hiddenNett");
+                        imgMedium.classList.add("hiddenNett");
+
+                        textStandard.classList.remove("hiddenNett");
+                        textPremium.classList.add("hiddenNett");
+                        textMedium.classList.add("hiddenNett");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        </script>
+
+
+        <?php socialLink($page); ?>
 
     </main>
 
