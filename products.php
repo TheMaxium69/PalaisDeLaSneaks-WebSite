a66d69c0f052d4083fac3dccfa4a499cebf97850
TheMaximeSan <mxm.tournier@gmail.com>
2023-10-11 14:09:45 +0200

    update

--- products.php
+++ products.php
@@ -25,45 +25,45 @@ head($page); ?>
         productOne($id);
     } else { ?>
 
-    <main>
-        <input id="search" type="text" class="form-control w-75 mx-auto" placeholder="Rechercher un porduit, marque...">
-        <div class="row">
-            <div class="col-2">
-            <h3 class="text-center mb-5">Trier par : </h3>
-                <div class="">
-                    <select class="form-select" aria-label="Default select example">
-                    <option selected>Taille</option>
-                    <option value="1">37</option>
-                    <option value="2">38</option>
-                    <option value="3">39</option>
-                    <option value="4">40</option>
-                    <option value="5">41</option>
-                    <option value="6">42</option>
-                    <option value="7">43</option>
-                    <option value="8">44</option>
-                    <option value="9">45</option>
-                    </select>
+        <main>
+            <input id="search" type="text" class="form-control w-75 mx-auto" placeholder="Rechercher un porduit, marque...">
+            <div class="row">
+                <div class="col-2">
+                    <h3 class="text-center mb-5">Trier par : </h3>
+                    <div class="">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>Taille</option>
+                            <option value="1">37</option>
+                            <option value="2">38</option>
+                            <option value="3">39</option>
+                            <option value="4">40</option>
+                            <option value="5">41</option>
+                            <option value="6">42</option>
+                            <option value="7">43</option>
+                            <option value="8">44</option>
+                            <option value="9">45</option>
+                        </select>
+                    </div>
+                    <div class="mt-2">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>Marque</option>
+                            <option value="1">Nike</option>
+                            <option value="2">Adidas</option>
+                            <option value="3">Yeezy</option>
+                            <option value="4">Jordan</option>
+                        </select>
+                    </div>
+                    <div class="mt-2">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>Prix</option>
+                            <option value="1">0€ - 50€</option>
+                            <option value="2">51€ - 100€</option>
+                            <option value="3">101€ - 150€</option>
+                            <option value="4">151€ - 200€</option>
+                            <option value="5">200€ et plus</option>
+                        </select>
+                    </div>
                 </div>
-                <div class="mt-2">
-                    <select class="form-select" aria-label="Default select example">
-                    <option selected>Marque</option>
-                    <option value="1">Nike</option>
-                    <option value="2">Adidas</option>
-                    <option value="3">Yeezy</option>
-                    <option value="4">Jordan</option>
-                    </select>
-                </div>
-                <div class="mt-2">
-                    <select class="form-select" aria-label="Default select example">
-                    <option selected>Prix</option>
-                    <option value="1">0€ - 50€</option>
-                    <option value="2">51€ - 100€</option>
-                    <option value="3">101€ - 150€</option>
-                    <option value="4">151€ - 200€</option>
-                    <option value="5">200€ et plus</option>
-                    </select>
-                </div>
-            </div>
 
                 <div class="container alert alert-light col-10">
                     <div class="row justify-content-around px-5" id="tableTEST">
@@ -81,12 +81,11 @@ head($page); ?>
                                     <p class="card-text text-center text-dark"><?= $product['pricing']['EUR']['monthly'] ?> €</p>
                                 </div>
                             </div>
-                        </div>
-                    <?php } ?>
+                        <?php } ?>
+                    </div>
                 </div>
             </div>
-    </div>
-        
+
 
         </main>
 
