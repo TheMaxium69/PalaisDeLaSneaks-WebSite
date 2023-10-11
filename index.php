b9f07579f3d81423e2e599a85da9551af009b7fe
TheMaximeSan <mxm.tournier@gmail.com>
2023-10-11 14:43:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -33,10 +33,10 @@ head($page); ?>
 
                     <div class="card" style="width: 18rem;">
                         <h3 class="p-4 text-center"><?= $products[$i]['name'] ?></h3>
-                        <p class="card-text text-center">MEN'S TRAINING SHOES</p>
+                        <p class="card-text text-center text-danger">Nike</p>
                         <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
                         <div class="card-body">
-                            <p class="card-text text-center">€ 130</p>
+                            <p class="card-text text-center bold">130 €</p>
                         </div>
                     </div>
 
