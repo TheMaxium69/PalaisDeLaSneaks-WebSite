4b69ae09943fed99802d2c86aa265c1d80835572
Nueve69 <yder09@gmail.com>
2023-10-12 10:13:48 +0200

    commit rachat

---
+++ rachat.php
@@ -0,0 +1,32 @@
+<?php include "app/app.php";
+$page = 6;
+head($page); ?>
+
+<body id="rachat">
+    <header> <?php navbar($page); ?> </header>
+
+
+    <main>
+
+<p>Rachat</p>
+
+</main>
+
+
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+<footer> <?php footer($page); ?> </footer>
+
+</html>
