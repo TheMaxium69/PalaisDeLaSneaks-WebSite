2091be591f7bf9b0477b36f730e0ded0250fb342
Nueve69 <yder09@gmail.com>
2023-10-16 09:33:52 +0200

    commit heu

--- rachat.php
+++ rachat.php
@@ -7,17 +7,18 @@ head($page); ?>
 
 
     <main>
-
     <h1 class="text-center underline">Revendez votre paire !</h1>
     <br>
     <br>
     <br>
-    <p class="text-center">vous souhaitez revendre votre paire ? contactez-nous directement sur notre messagerie Instagram, notre équipe se chargera d'estimé et vous proposez le meilleur prix pour vous<p>
-    <div class="container text-center">
-    <a name="" id="" class="btn btn-primary underline" href="https://www.instagram.com/palais.de.la.sneaks/" role="button">je revends ma paire !</a>
+  <div class="container text-center">
+    <div class="mx-auto border p-3" style="max-width: 50%;">
+      <p class="text-center">Vous souhaitez revendre votre paire ? Contactez-nous directement sur notre messagerie Instagram, notre équipe se chargera d'estimer et vous proposer le meilleur prix pour vous.</p>
+      <a name="" id="" class="btn btn-primary underline" href="https://www.instagram.com/palais.de.la.sneaks/" role="button">Je revends ma paire !</a>
+    </div>
   </div>
 
-<?php socialLink($page); ?>
+  <?php socialLink($page); ?>
 
 </main>
 
