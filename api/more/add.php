0272bed241562e69e77b76665338d1fc3daf1981
TheMaximeSan <mxm.tournier@gmail.com>
2023-10-17 10:41:01 +0200

    update

--- api/more/add.php
+++ api/more/add.php
@@ -1,76 +1,93 @@
-<?php
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<div style="text-align: center" class="alert alert-<?php
 
 require_once "upload.php";
-require_once "form.phtml";
 
 //var_dump($_POST);
 //
 //var_dump($_FILES);
+if(isset($_POST["submit"])) {
+    $target_dir = "uploads/";
+    $pid = $_POST['pid'];
+    $nbImage = 0;
+    if ($_FILES["fileToUpload1"]["tmp_name"]) {
+        $nbImage = $nbImage + 1;
+    }
+    if ($_FILES["fileToUpload2"]["tmp_name"]) {
+        $nbImage = $nbImage + 1;
+    }
+    if ($_FILES["fileToUpload3"]["tmp_name"]) {
+        $nbImage = $nbImage + 1;
+    }
 
-$target_dir = "uploads/";
-$pid = $_POST['pid'];
-$nbImage = 0;
-if ($_FILES["fileToUpload1"]["tmp_name"]){ $nbImage = $nbImage + 1; }
-if ($_FILES["fileToUpload2"]["tmp_name"]){ $nbImage = $nbImage + 1; }
-if ($_FILES["fileToUpload3"]["tmp_name"]){ $nbImage = $nbImage + 1; }
+    if ($nbImage !== 0 && !empty($pid)) {
 
-if ($nbImage !== 0 && !empty($pid)){
+        for ($i = 1; $i <= $nbImage; $i++) {
 
-    for ($i = 1; $i <= $nbImage; $i++){
+            $uploadOk = 1;
 
-        $uploadOk = 1;
+            $namePicture = basename(randomName());
+            $target_file = $target_dir . basename($_FILES["fileToUpload" . $i]["name"]);
+            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+            $namePictureWithExtend = basename($namePicture) . "." . $imageFileType;
+            $target_fileUpload = $target_dir . $namePictureWithExtend;
 
-        $namePicture = basename(randomName());
-        $target_file = $target_dir . basename($_FILES["fileToUpload". $i]["name"]);
-        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-        $namePictureWithExtend = basename($namePicture) . "." . $imageFileType;
-        $target_fileUpload = $target_dir . $namePictureWithExtend;
+            // Check if image file is a actual image or fake image
+            if (isset($_POST["submit"])) {
+                $check = getimagesize($_FILES["fileToUpload" . $i]["tmp_name"]);
+                if ($check !== false) {
+                    $uploadOk = 1;
+                } else {
+                    $uploadOk = 0;
+                }
+            }
 
-        // Check if image file is a actual image or fake image
-        if(isset($_POST["submit"])) {
-            $check = getimagesize($_FILES["fileToUpload". $i]["tmp_name"]);
-            if($check !== false) {
-                echo "File is an image - " . $check["mime"] . ".";
-                $uploadOk = 1;
-            } else {
-                echo "File is not an image.";
+            // Check file size
+            if ($_FILES["fileToUpload" . $i]["size"] > 5000000) {
+                echo 'danger"> x( Désolé, Votre fichier est trop lourd.';
                 $uploadOk = 0;
             }
-        }
 
-        // Check file size
-        if ($_FILES["fileToUpload".$i]["size"] > 50000000000) {
-            echo "Sorry, your file is too large.";
-            $uploadOk = 0;
-        }
+            // Allow certain file formats
+            if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                && $imageFileType != "gif") {
+                if ($uploadOk == 1){
+                    echo 'danger"> x(';
+                } else {
+                    echo '<br>';
+                }
+                echo 'Désolé, uniquement JPG, JPEG, PNG & GIF sont autorisé.';
+                $uploadOk = 0;
+            }
 
-        // Allow certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-            && $imageFileType != "gif" ) {
-            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-            $uploadOk = 0;
-        }
-
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded.";
-        // if everything is ok, try to upload file
-        } else {
-            if (move_uploaded_file($_FILES["fileToUpload". $i]["tmp_name"], $target_fileUpload)) {
-                echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload". $i]["name"])). " has been uploaded.";
-                uploadPicture($pid, $i, $namePictureWithExtend);
-            } else if (file_exists($target_fileUpload)) {
-                echo "<strong>Sorry, file already exists.</strong>";
-//                uploadPicture($pid, $i, $namePictureWithExtend);
+            // Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+                echo "<br><small> votre fichier n'est pas envoyé</small>";
+                // if everything is ok, try to upload file
             } else {
-                echo "Sorry, there was an error uploading your file.";
+                if (move_uploaded_file($_FILES["fileToUpload" . $i]["tmp_name"], $target_fileUpload)) {
+                    if ($i == 1){
+                        echo "success\">";
+                    } else {
+                        echo "<br> et";
+                    }
+                    echo " Votre fichier " . htmlspecialchars(basename($_FILES["fileToUpload" . $i]["name"])) . " a bien été upload.";
+                    uploadPicture($pid, $i, $namePictureWithExtend);
+                } else if (file_exists($target_fileUpload)) {
+                    echo 'danger"> x( Le fichier existe déjà veuillez réessayer.';
+//                uploadPicture($pid, $i, $namePictureWithExtend);
+                } else {
+                    echo 'danger"> x( Désolé, un problème est survenue lors de l\'upload.';
+                }
             }
         }
+    } else {
+        echo 'danger"> x( désolé votre fichier n\'a pas été upload.';
     }
 } else {
-    echo "Sorry, your file was not uploaded.";
+    echo 'secondary"> Ici vous pouvez mettre les images de vos chaussure';
 }
-
 
 function randomName($length=20){
     $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -80,3 +97,11 @@ function randomName($length=20){
     }
     return $string;
 }
+
+?>
+</div>
+<?php
+
+require_once "form.phtml";
+
+?>
--- api/size/add.php
+++ api/size/add.php
@@ -1,8 +1,6 @@
-<?php
-
-require_once "form.phtml";
-
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<div style="text-align: center" class="alert alert-<?php
 
 if(isset($_POST["submit"]) && $_POST['submit'] == 'Upload Size') {
 
@@ -24,9 +22,15 @@ if(isset($_POST["submit"]) && $_POST['submit'] == 'Upload Size') {
     $jsonCount = json_encode($counts);
     $jsonSize = json_encode($sizes);
 
+
     if($pid != 0){
         uploadSize($pid, $jsonCount, $jsonSize);
+    }else{
+
+        echo 'danger"> x( vous avez selectionnez aucune chaussure';
     }
+} else {
+    echo 'secondary"> Ici vous pouvez noté la taille de vos chaussure';
 }
 
 function uploadSize($pid, $count, $size)
@@ -45,7 +49,7 @@ function uploadSize($pid, $count, $size)
             'count' => $count,
             'size' => $size
         ]);
-        echo "Bien modifier";
+        echo 'success"> Bien modifier';
     }else{
         $q = $db->prepare("INSERT INTO size (pid,count,size) VALUES(:pid,:count,:size)");
         $q->execute([
@@ -54,6 +58,13 @@ function uploadSize($pid, $count, $size)
             'size' => $size
 
         ]);
-        echo "Taille bien noté";
+        echo 'success"> Taille bien noté';
     }
 }
+?>
+</div>
+<?php
+
+require_once "form.phtml";
+
+?>
