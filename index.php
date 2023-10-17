b4ae6bf13c16f7d83d3b999e6fc7ee89f5ddc8ac
Marilyne Naili <marilyne.lydia@gmail.com>
2023-10-17 10:39:25 +0200

    update section nettoyage

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ head($page); ?>
   </div>
 
   <section id="gallery">
-            <h3 class="text-center title">N O U V E A U T E</h3>
+            <h3 class="text-center title">N O U V E A U T E S</h3>
 
             <div class="row justify-content-center px-5">
 
@@ -30,12 +30,25 @@ head($page); ?>
                 for ($i = 0; $i <= 9; $i++) {
                 ?>
 
-                    <div class="card" style="width: 18rem;" onclick="window.location.href = 'products.php?p=<?= $products[$i]['pid'] ?>';">
+                    <!-- <div class="card" style="width: 18rem;" onclick="window.location.href = 'products.php?p=<?= $products[$i]['pid'] ?>';">
                         <h3 class="p-4 text-center"><?= $products[$i]['name'] ?></h3>
                         <p class="card-text text-center text-danger">Nike</p>
                         <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="Card image cap">
                         <div class="card-body">
                             <p class="card-text text-center bold">130 €</p>
+                        </div>
+                    </div> -->
+
+                    <div class="card card__one" onclick="window.location.href = '?p=<?= $product['pid'] ?>';">
+                        <div class="product-image">
+                            <!-- <img src="app/assets/sneakersLV.png" alt="OFF-white Red Edition" draggable="false" /> -->
+                            <!-- <img class="card-img-top" src="api/more/uploads/<?= $productPicture['picture1'] ?>" alt="Card image cap"> -->
+                            <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/2358/2817/products/vaporwaffle-sacai-black-white-131891.png?v=1638814653" alt="">
+                        </div>
+                        <div class="product-info">
+                            <h2 class="nameSneakers"><?= $products[$i]['name'] ?></h2>
+                            <h6 class="nameMarque">Nike</h6>
+                            <div class="price"><?= $product['pricing']['EUR']['monthly'] ?> €</div>
                         </div>
                     </div>
 
