14ad11e8b187df74eb04edc395b5697022ec59dc
Marilyne Naili <marilyne.lydia@gmail.com>
2023-10-17 12:19:04 +0200

    update style rachat.php

--- rachat.php
+++ rachat.php
@@ -2,25 +2,29 @@
 $page = 6;
 head($page); ?>
 
-<body id="rachat">
+<body>
     <header> <?php navbar($page); ?> </header>
 
 
-    <main>
-    <h1 class="text-center underline">Revendez votre paire !</h1>
-    <br>
-    <br>
-    <br>
-  <div class="container text-center">
-    <div class="mx-auto border p-3" style="max-width: 50%;">
-      <p class="text-center">Vous souhaitez revendre votre paire ? Contactez-nous directement sur notre messagerie Instagram, notre équipe se chargera d'estimer et vous proposer le meilleur prix pour vous.</p>
-      <a name="" id="" class="btn btn-primary underline" href="https://www.instagram.com/palais.de.la.sneaks/" role="button">Je revends ma paire !</a>
+  <main id="rachat">
+    <h1 class="text-center m-5">Revendez votre paire !</h1>
+
+    <div class="cardRevente">
+      <div class="w-50 picture">
+      </div>
+      <div class="w-50 cardRight">
+        <div class="text">
+          <p>Vous souhaitez revendre votre paire ?</p>
+           <p>Contactez-nous directement sur notre messagerie Instagram, notre équipe se chargera d'estimer et vous proposer le meilleur prix pour vous.</p>
+          <!-- <a name="" id="" class="btn btn-primary underline" href="https://www.instagram.com/palais.de.la.sneaks/" role="button">Je revends ma paire !</a> -->
+          <button class="custom-btn btn-2">Revendre ma paire</button>
+        </div>
     </div>
   </div>
 
   <?php socialLink($page); ?>
 
-</main>
+  </main>
 
 
 
