4122bb1a981ff4bafa7a1a3b31dd65fbacbe151f
Nueve69 <yder09@gmail.com>
2023-10-18 11:59:22 +0200

    commit rachat majj

--- rachat.php
+++ rachat.php
@@ -10,13 +10,13 @@ head($page); ?>
   <h1 style="background-color:#dc3545; color: #ffffff; text-transform: uppercase; font-weight: bold; text-align: center;padding:10px;font-size:24px;margin: 0">Nous achetons vos paires au meilleur prix</h1>
     <div class="cardRevente">
     </div>
+    <br>
 
       <div>
-        <div class="text">
+        <div class="text" style="margin: auto;width: 50%;text-align: center" >
           <h2>Vous souhaitez revendre votre paire ?</h2>
            <p>Contactez-nous directement sur notre messagerie Instagram, notre équipe se chargera d'estimer et vous proposer le meilleur prix pour vous.</p>
-          <button class="custom-btn btn-2">Revendre ma paire</button>
-        </div>
+           <a href="https://www.instagram.com/palais.de.la.sneaks/" class="btn btn-lg" title="Conditions générales de rachat" style="postion:relative;background-color:#b6050f; text-transform:uppercase; color:#fff;border-radius: 5px; font-size: 14px; font-weight:bold">Je veux revendre ma paire</a>        </div>
     </div>
   <?php socialLink($page); ?>
 
